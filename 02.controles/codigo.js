616c1f1a3b1aaff9e3194bd71274504b12fd5135
Ruben Catalon <rubencatalonorza2@gmail.com>
2026-09-25 11:08:25 +0200

    Creacion del ejercicio 1 y ampliando la informacion

---
+++ 02.controles/codigo.js
@@ -0,0 +1,38 @@
+//Codigo
+var cosa = 3;
+console.log(cosa++); // 3 y después incrementa a 4
+cosa = 3;
+console.log(++cosa); // incrementa a 4 y muestra 4
+
+//Ejemplo de bucles
+function howMany(selectObject) {
+    let numberSelected = 0;
+    for (let i = 0; i < selectObject.options.length; i++) {
+        if (selectObject.options[i].selected) {
+        numberSelected++;
+        }
+    }
+    return numberSelected;
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+        alert(
+            "Número de opciones seleccionadas: " +
+            howMany(document.selectForm.musicTypes), //document.getEle,entById("musicTypes")
+        );
+});
+
+//Ejemplo mover options de un select a otro
+let boton2 = document.getElementById("btn2");
+boton2.addEventListener("click",
+    function () {
+        let s1 = document.getElementById("musicTypes");
+        let s2 = document.getElementById("vacio");
+        for(let i = s1.options.length - 1;i = 0;i++){
+            if (s1.options[1].selected) {
+                s2.appendChild(s1.options[i].values)
+            }
+        }
+    }
+);
---
+++ 02.controles/tarea1/ejer1.js
@@ -0,0 +1,6 @@
+//Tabla de multiplicar
+var num = 2;
+console.log("Tabla del "+ num);
+for(i = 0;i <= 10;i++){
+    console.log(num + " x " + i + + " = " + (num*i));
+}
